77fdde383dfe093c5eadcd21dac4004b67f1fba9
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2017-11-06 23:29:36 +0500

    Copy code from default matcher to fast matcher

--- fastmatcher.go
+++ fastmatcher.go
@@ -1,5 +1,10 @@
 package hlconf2017
 
+import (
+	"regexp"
+	"strings"
+)
+
 // FastPatternMatcher implements high-performance Graphite metric filtering
 type FastPatternMatcher struct {
 	AllowedPatterns []string
@@ -11,11 +16,34 @@ type FastPatternMatcher struct {
 //   metric.name.wild*card
 //   metric.name.{one,two}.maybe.longer
 func (p *FastPatternMatcher) InitPatterns(allowedPatterns []string) {
-	// your own fast implementation here
+	p.AllowedPatterns = allowedPatterns
 }
 
 // DetectMatchingPatterns returns a list of allowed patterns that match given metric
 func (p *FastPatternMatcher) DetectMatchingPatterns(metricName string) (matchingPatterns []string) {
-	// your own fast implementation here
+	metricParts := strings.Split(metricName, ".")
+
+NEXTPATTERN:
+	for _, pattern := range p.AllowedPatterns {
+		patternParts := strings.Split(pattern, ".")
+		if len(patternParts) != len(metricParts) {
+			continue NEXTPATTERN
+		}
+		for i, part := range patternParts {
+			regexPart := "^" + part + "$"
+			regexPart = strings.Replace(regexPart, "*", ".*", -1)
+			regexPart = strings.Replace(regexPart, "{", "(", -1)
+			regexPart = strings.Replace(regexPart, "}", ")", -1)
+			regexPart = strings.Replace(regexPart, ",", "|", -1)
+
+			regex := regexp.MustCompile(regexPart)
+
+			if !regex.MatchString(metricParts[i]) {
+				continue NEXTPATTERN
+			}
+		}
+		matchingPatterns = append(matchingPatterns, pattern)
+	}
+
 	return
 }
