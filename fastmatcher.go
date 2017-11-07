cda00fefdc3f1697d96748e50f4324ae1865c3af
Nikolay Mikolaychuk <n.mikolaichuk@propellerads.net>
2017-11-08 00:01:35 +0300

    dev

--- fastmatcher.go
+++ fastmatcher.go
@@ -8,6 +8,7 @@ import (
 // FastPatternMatcher implements high-performance Graphite metric filtering
 type (
 	FastPatternMatcher struct {
+		Checkers [][]Checker
 		Patterns [][]Pattern
 		Count    int
 	}
@@ -15,14 +16,23 @@ type (
 	RegexpItem struct {
 		isEmpty  bool
 		isRegexp bool
-		reg      *regexp.Regexp
+		reg      func(string) bool
 		str      string
+		pat      string
+		pattern  string
 	}
 
 	Pattern struct {
 		count int
 		r     []RegexpItem
 		str   string
+		parts []string
+	}
+
+	Checker struct {
+		level    int
+		rItem    *RegexpItem
+		children []Checker
 	}
 )
 
@@ -33,6 +43,46 @@ type (
 //   metric.name.{one,two}.maybe.longer
 func (p *FastPatternMatcher) InitPatterns(allowedPatterns []string) {
 	p.Patterns = make([][]Pattern, 256)
+	p.Checkers = make([][]Checker, 256)
+	re := regexp.MustCompile("^([^{]+)({[^}]+})(.*)$")
+
+	patternsMap := map[string]string{}
+
+	for {
+		replaced := false
+		tmpAllowedPatterns := make([]string, 0)
+		for _, pattern := range allowedPatterns {
+			matches := re.FindAllStringSubmatch(pattern, -1)
+			if len(matches) == 1 {
+				matches[0][2] = strings.Replace(matches[0][2], "{", "", -1)
+				matches[0][2] = strings.Replace(matches[0][2], "}", "", -1)
+
+				tokens := strings.Split(matches[0][2], ",")
+
+				for _, token := range tokens {
+					tmpAllowedPatterns = append(tmpAllowedPatterns, matches[0][1]+token+matches[0][3])
+
+					if _, ok := patternsMap[matches[0][1]+token+matches[0][3]]; !ok {
+						patternsMap[matches[0][1]+token+matches[0][3]] = pattern
+					}
+				}
+
+				replaced = true
+			} else {
+				tmpAllowedPatterns = append(tmpAllowedPatterns, pattern)
+				if _, ok := patternsMap[pattern]; !ok {
+					patternsMap[pattern] = pattern
+				}
+			}
+		}
+
+		allowedPatterns = tmpAllowedPatterns
+
+		if !replaced {
+			break
+		}
+	}
+
 	p.Count = len(allowedPatterns)
 
 	for _, pattern := range allowedPatterns {
@@ -43,25 +93,63 @@ func (p *FastPatternMatcher) InitPatterns(allowedPatterns []string) {
 
 		for j, patternPart := range patternParts {
 			regexPart := strings.Replace(patternPart, "*", ".*", -1)
-			regexPart = strings.Replace(regexPart, "{", "(", -1)
-			regexPart = strings.Replace(regexPart, "}", ")", -1)
-			regexPart = strings.Replace(regexPart, ",", "|", -1)
 
+			lastPattern := ""
+			if j == len(patternParts)-1 {
+				lastPattern = patternsMap[pattern]
+			}
+
+			pat := patternPart
 			if patternPart == "*" {
 				r[j] = RegexpItem{
 					isEmpty: true,
 					str:     patternPart,
+					pat:     pat,
+					pattern: lastPattern,
 				}
 			} else if regexPart == patternPart {
 				r[j] = RegexpItem{
 					isRegexp: false,
 					str:      patternPart,
+					pat:      pat,
+					pattern:  lastPattern,
 				}
 			} else {
 				regexPart = "^" + regexPart + "$"
 				r[j] = RegexpItem{
 					isRegexp: true,
-					reg:      regexp.MustCompile(regexPart),
+					pat:      pat,
+					pattern:  lastPattern,
+					reg: func(subj string) bool {
+						parts := strings.Split(pat, "*")
+
+						if len(parts) == 1 {
+							return subj == pat
+						}
+
+						leadingGlob := strings.HasPrefix(pat, "*")
+						trailingGlob := strings.HasSuffix(pat, "*")
+						end := len(parts) - 1
+
+						for i := 0; i < end; i++ {
+							idx := strings.Index(subj, parts[i])
+
+							switch i {
+							case 0:
+								if !leadingGlob && idx != 0 {
+									return false
+								}
+							default:
+								if idx < 0 {
+									return false
+								}
+							}
+
+							subj = subj[idx+len(parts[i]):]
+						}
+
+						return trailingGlob || strings.HasSuffix(subj, parts[end])
+					},
 				}
 			}
 
@@ -75,8 +163,83 @@ func (p *FastPatternMatcher) InitPatterns(allowedPatterns []string) {
 			count: count,
 			r:     r,
 			str:   pattern,
+			parts: patternParts,
 		})
 	}
+
+	for i := 0; i < len(p.Patterns); i++ {
+		if p.Patterns[i] == nil {
+			continue
+		}
+
+		for j := 0; j < len(p.Patterns[i]); j++ {
+			if p.Checkers[p.Patterns[i][j].count] == nil {
+				p.Checkers[p.Patterns[i][j].count] = make([]Checker, 0)
+			}
+
+			list := &p.Checkers[p.Patterns[i][j].count]
+
+			for q := 0; q < len(p.Patterns[i][j].r); q++ {
+				list = addChecker(list, q, &p.Patterns[i][j].r[q])
+			}
+		}
+	}
+}
+
+func addChecker(arr *[]Checker, level int, rItem *RegexpItem) *[]Checker {
+	for i := 0; i < len(*arr); i++ {
+		if (*arr)[i].rItem.pat == rItem.pat && level == (*arr)[i].level {
+			return &(*arr)[i].children
+		}
+	}
+
+	*arr = append(*arr, Checker{
+		level:    level,
+		rItem:    rItem,
+		children: make([]Checker, 0),
+	})
+
+	return &(*arr)[len(*arr)-1].children
+}
+
+func getMatches(arr *[]Checker, metric *[]string) []string {
+	matches := make([]string, 0)
+	for i := 0; i < len(*arr); i++ {
+		if (*arr)[i].level >= len(*metric) {
+			return matches
+		}
+
+		if (*arr)[i].rItem.isEmpty {
+			if len(*metric)-1 == (*arr)[i].level {
+				if (*arr)[i].rItem.pattern != "" {
+					matches = append(matches, (*arr)[i].rItem.pattern)
+				}
+			} else {
+				matches = append(matches, getMatches(&(*arr)[i].children, metric)...)
+			}
+		} else if (*arr)[i].rItem.isRegexp {
+			if (*arr)[i].rItem.reg((*metric)[(*arr)[i].level]) {
+				if len(*metric)-1 == (*arr)[i].level {
+					if (*arr)[i].rItem.pattern != "" {
+						matches = append(matches, (*arr)[i].rItem.pattern)
+					}
+				} else {
+					matches = append(matches, getMatches(&(*arr)[i].children, metric)...)
+				}
+			}
+		} else {
+			if (*arr)[i].rItem.str == (*metric)[(*arr)[i].level] {
+				if len(*metric)-1 == (*arr)[i].level {
+					if (*arr)[i].rItem.pattern != "" {
+						matches = append(matches, (*arr)[i].rItem.pattern)
+					}
+				} else {
+					matches = append(matches, getMatches(&(*arr)[i].children, metric)...)
+				}
+			}
+		}
+	}
+	return matches
 }
 
 // DetectMatchingPatterns returns a list of allowed patterns that match given metric
@@ -86,38 +249,7 @@ func (p *FastPatternMatcher) DetectMatchingPatterns(metricName string) (matching
 	}
 
 	metric := strings.Split(metricName, ".")
-
-	q := 0
-	matchingPatterns = make([]string, p.Count)
-UP:
-	for i := 0; i < len(p.Patterns[metricName[0]]); i++ {
-		if p.Patterns[metricName[0]][i].count != len(metric) {
-			continue
-		}
-
-		for j := 0; j < p.Patterns[metricName[0]][i].count; j++ {
-			if p.Patterns[metricName[0]][i].r[j].isEmpty {
-				continue
-			} else if p.Patterns[metricName[0]][i].r[j].isRegexp {
-				if !p.Patterns[metricName[0]][i].r[j].reg.MatchString(metric[j]) {
-					continue UP
-				}
-			} else {
-				if p.Patterns[metricName[0]][i].r[j].str != metric[j] {
-					continue UP
-				}
-			}
-		}
-
-		matchingPatterns[q] = p.Patterns[metricName[0]][i].str
-		q++
-	}
-
-	if q > 0 {
-		matchingPatterns = matchingPatterns[0:q]
-	} else {
-		matchingPatterns = make([]string, 0)
-	}
+	matchingPatterns = getMatches(&p.Checkers[len(metric)], &metric)
 
 	return
 }
