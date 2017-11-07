1078df884a2c93c66cb344c4379ce54023450c92
Artem Kovardin <horechek@gmail.com>
2017-11-07 22:15:00 +0300

    one regexp

--- fastmatcher.go
+++ fastmatcher.go
@@ -14,6 +14,8 @@ type Pattern struct {
 	Part3 Part
 
 	Parts []Part
+
+	UberRgs *regexp.Regexp
 }
 
 type Part struct {
@@ -35,64 +37,70 @@ func (p *FastPatternMatcher) InitPatterns(allowedPatterns []string) {
 	p.P = make([]Pattern, len(allowedPatterns))
 
 	for i, pattern := range allowedPatterns {
-		for _, part := range strings.Split(pattern, ".") {
-			regexPart := "^" + part + "$"
+		str := "^"
+		for j, part := range strings.Split(pattern, ".") {
+			regexPart := part
 			regexPart = strings.Replace(regexPart, "*", ".*", -1)
 			regexPart = strings.Replace(regexPart, "{", "(", -1)
 			regexPart = strings.Replace(regexPart, "}", ")", -1)
 			regexPart = strings.Replace(regexPart, ",", "|", -1)
 
+			inner := regexPart
+
+			regexPart = "^" + regexPart + "$"
+
 			p.P[i].Parts = append(p.P[i].Parts, Part{
 				Part: part,
 				Rgs:  regexp.MustCompile(regexPart),
 			})
+
+			if j != 0 {
+				str += `\.` + inner
+			} else {
+				str += inner
+			}
+
 		}
 
+		str += "$"
+
 		p.P[i].Len = len(p.P[i].Parts)
+		p.P[i].Full = pattern
 
 		p.P[i].Part1 = p.P[i].Parts[0]
 		p.P[i].Part2 = p.P[i].Parts[1]
 		if len(p.P[i].Parts) == 3 {
 			p.P[i].Part3 = p.P[i].Parts[2]
 		}
+
+		p.P[i].UberRgs = regexp.MustCompile(str)
 	}
 }
 
 var (
-	matchingPatterns = make([]string, 0, 50)
+	matchingPatterns = make([]string, 100)
 	l                int
 	metricParts      []string
-	pos              int
 )
 
 // DetectMatchingPatterns returns a list of allowed patterns that match given metric
-func (p *FastPatternMatcher) DetectMatchingPatterns(metricName string) []string {
+func (p FastPatternMatcher) DetectMatchingPatterns(metricName string) []string {
 	metricParts = strings.Split(metricName, ".")
-	matchingPatterns = []string{}
+	matchingPatterns = make([]string, 0, len(p.P))
 	l = len(metricParts)
 
-	pos = 0
 	for _, pt := range p.P {
 		if pt.Len != l {
 			continue
 		}
 
-		if l == 3 {
-			if !pt.Part3.Rgs.MatchString(metricParts[2]) {
-				continue
-			}
-		}
 
-		if !pt.Part2.Rgs.MatchString(metricParts[0]) {
+		if !pt.UberRgs.MatchString(metricName) {
 			continue
 		}
 
-		if !pt.Part1.Rgs.MatchString(metricParts[1]) {
-			continue
-		}
 
-		matchingPatterns[pos] = pt.Full
-		pos++
+		matchingPatterns = append(matchingPatterns, pt.Full)
 	}
 
 	return matchingPatterns
