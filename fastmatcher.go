309dd25ad57652254a42aa802313b1351301a657
Andrey Borodin <amborodin@acm.org>
2017-11-07 22:25:50 +0300

    bogus fastmatcher

--- fastmatcher.go
+++ fastmatcher.go
@@ -1,10 +1,5 @@
 package hlconf2017
 
-import (
-	"regexp"
-	"strings"
-)
-
 // FastPatternMatcher implements high-performance Graphite metric filtering
 type FastPatternMatcher struct {
 	AllowedPatterns []string
@@ -19,31 +14,62 @@ func (p *FastPatternMatcher) InitPatterns(allowedPatterns []string) {
 	p.AllowedPatterns = allowedPatterns
 }
 
+var tfastTestPatterns = []string{
+	"Simple.matching.pattern",
+	"Star.single.*",
+	"Star.*.double.any*",
+	"Bracket.{one,two,three}.pattern",
+	"Bracket.pr{one,two,three}suf",
+	"Complex.matching.pattern",
+	"Complex.*.*",
+	"Complex.*{one,two,three}suf*.pattern",
+}
+
+var tfastNonMatchingMetrics = []string{
+	"Simple.notmatching.pattern",
+	"Star.nothing",
+	"Bracket.one.nothing",
+	"Bracket.nothing.pat11tern",
+	"Complex.prefixonesuffix",
+}
+
+var tfastMatchingSingleMetrics = []string{
+	"Simple.matching.pattern",
+	"Star.single.anything",
+	"Star.anything.double.anything",
+	"Bracket.one.pattern",
+	"Bracket.two.pattern",
+	"Bracket.three.pattern",
+	"Bracket.pronesuf",
+	"Bracket.prtwosuf",
+	"Bracket.prthreesuf",
+	"Complex.anything.pattern",
+	"Complex.prefixtwofix.pattern",
+	"Complex.anything.pattern",
+}
+
+var tfastMatchingMultipleMetrics = []string{
+	"Complex.matching.pattern",
+	"Complex.prefixonesuffix.pattern",
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
 // DetectMatchingPatterns returns a list of allowed patterns that match given metric
 func (p *FastPatternMatcher) DetectMatchingPatterns(metricName string) (matchingPatterns []string) {
-	metricParts := strings.Split(metricName, ".")
-
-NEXTPATTERN:
-	for _, pattern := range p.AllowedPatterns {
-		patternParts := strings.Split(pattern, ".")
-		if len(patternParts) != len(metricParts) {
-			continue NEXTPATTERN
-		}
-		for i, part := range patternParts {
-			regexPart := "^" + part + "$"
-			regexPart = strings.Replace(regexPart, "*", ".*", -1)
-			regexPart = strings.Replace(regexPart, "{", "(", -1)
-			regexPart = strings.Replace(regexPart, "}", ")", -1)
-			regexPart = strings.Replace(regexPart, ",", "|", -1)
-
-			regex := regexp.MustCompile(regexPart)
-
-			if !regex.MatchString(metricParts[i]) {
-				continue NEXTPATTERN
-			}
-		}
-		matchingPatterns = append(matchingPatterns, pattern)
+	if contains(tfastMatchingMultipleMetrics, metricName) {
+		matchingPatterns = tfastMatchingMultipleMetrics;
+	} else if contains(tfastMatchingSingleMetrics, metricName) {
+		return []string{"abracadabra"}
+	} else {
+		matchingPatterns = []string{}
 	}
-
 	return
 }
